6bfc9bf34b84643c0407a88e501048a1257c457f
Andrey Orlov <for_orlov@mail.ru>
2024-12-22 18:11:14 +0300

    clean

--- src/app/layouts/default-layout/default-layout.component.ts
+++ src/app/layouts/default-layout/default-layout.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../../components/header/header.component';
-import { AsideComponent } from '../../components/aside/aside.component';
 
 @Component({
   selector: 'app-default-layout',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, AsideComponent],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './default-layout.component.html',
   styleUrl: './default-layout.component.scss',
 })
